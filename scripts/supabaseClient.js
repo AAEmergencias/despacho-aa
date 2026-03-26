652657a790403b33ee5b69aaeb38197237e1551a
AA <centralemergenciaslosbronces@gmail.com>
2026-03-26 19:56:10 -0300

    Update supabaseClient.js

--- scripts/supabaseClient.js
+++ scripts/supabaseClient.js
@@ -1,10 +1,6 @@
-// URL del proyecto Supabase
-window.SUPABASE_URL = "https://jzfmzzwluqbithqatgbtds.supabase.co";
-
-// Llave ANON pública
+window.SUPABASE_URL = "https://jzfmzzwluqbithqbtgtds.supabase.co";
 window.SUPABASE_ANON_KEY = "sb-publishable_Fo0MKlp7_EzW-Ar6U-zpQA_CqKYYpy7";
 
-// Cliente Supabase V2
 const supabaseClient = supabase.createClient(
     window.SUPABASE_URL,
     window.SUPABASE_ANON_KEY
