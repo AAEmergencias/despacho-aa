906ebadaafeda437e0696469acaecfd24d015038
AA <centralemergenciaslosbronces@gmail.com>
2026-03-27 08:26:48 -0300

    Update supabaseClient.js

--- scripts/supabaseClient.js
+++ scripts/supabaseClient.js
@@ -3,7 +3,8 @@
 // ===============================
 
 window.SUPABASE_URL = "https://jzfmzzwluqbihtagbtds.supabase.co";
-window.SUPABASE_ANON_KEY = "sb-publishable_Fo0MKlp7_EzW-Ar6U-zpQA_CqKYYPy7";
+
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6Zm16endsdXFiaWh0YWdidGRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1MjI2MzgsImV4cCI6MjA5MDA5ODYzOH0.VB7kVCLobmRmKxXI0e0BoSr4mAmb5wpYQsS_fOUUZZ4";
 
 // Crear cliente Supabase V2
 const supabaseClient = supabase.createClient(
